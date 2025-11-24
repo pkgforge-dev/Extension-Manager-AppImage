753ea27bb8b1483cc3297314d5408c254212507b
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-11-24 14:36:36 +0100

    Add Vulkan and OpenGL deployment exports

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,8 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/com.mattjakeman.ExtensionManager.svg
 export DESKTOP=/usr/share/applications/com.mattjakeman.ExtensionManager.desktop
+export DEPLOY_VULKAN=1
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun  /usr/bin/extension-manager \
